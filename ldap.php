d37767ecbdcfb37ea2175d56a96cd711073c8d8c
j0se <jose.ferreira@gmail.com>
2013-07-18 12:11:47 +0200

    Create ldap.php

---
+++ ldap.php
@@ -0,0 +1,39 @@
+/*
+ *
+ * Work with CodeIgniter / Windows 2k8 AD
+ *
+ * Place this file @ application/libraries/Ldap.php
+ *
+ * Create by: j0se <jose -DOT- ferreira @ gmail.NO.SPAM
+ *
+ */
+ 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ldap {
+	public function validar($usuario,$password) {
+		$dominio = '<myDOMAIN>';
+		$servidor = '<LDAP_SERVER>';
+		$ldaprdn =  $dominio . "\\" . $usuario;
+		$ldappass = $password;
+		$ldapconn = ldap_connect($servidor) 
+		    or die("Could not connect to LDAP server."); /* Connect or die this is the question!! */
+		if ($ldapconn)  {
+		    $ldapbind = @ldap_bind($ldapconn, $ldaprdn, $ldappass); /* Connect with user and pass */
+		    if (($ldapbind) and ($password != '')) {
+		        $estado = 'OK'; /* User and Password is OK. All is OK */
+		    } else {
+		        $estado = 'ERROR'; /* WRONG!!! */
+		    }
+		}
+		ldap_close($ldapconn); /*Close*/
+		return $estado;
+	}
+	public function tiene_session() {
+		$CI =& get_instance();
+  		$tiene_session = $CI->session->userdata('login_on');
+    	if(!isset($tiene_session) || $tiene_session != TRUE) {
+    			 redirect('login');    
+    	} 
+	}
+}
